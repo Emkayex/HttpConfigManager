754a613e4b4f15748200e78cb73a399a7eb9b682
Emkayex <53287857+Emkayex@users.noreply.github.com>
2024-03-13 21:53:08 -0500

    Added Temporary Stylesheets

--- Server/HtmlGenerator.cs
+++ Server/HtmlGenerator.cs
@@ -78,6 +78,10 @@ public static class HtmlGenerator
 <head>
     <title>{title}</title>
 
+    <link rel=""stylesheet"" href=""https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"" />
+    <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"" />
+    <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"" />
+
     {string.Join("\n    ", IterateScriptTags())}
 </head>
 ";
